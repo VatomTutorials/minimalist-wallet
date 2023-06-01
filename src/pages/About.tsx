081edc93574fc49623ff12e2868cd37ec6a7c56f
Joe Sandmeyer <github.com@unconscious.com>
2023-06-01 19:30:34 -0400

    Common torso for pages

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,11 +1,20 @@
 import { Link } from "react-router-dom";
+import appLogo from '../assets/AppLogo.png'
 import './Common.css';
 //
 const AboutPage = () => {
   return (
-		<div>
-			<h1>Welcome to the About page</h1>
-			<Link to="/home"> Return to the Home page </Link>
+		<div className="page_wrap_gp">
+			<div className="page_torso_gp">
+				<img src={appLogo} alt="app logo" />
+			</div>
+			<div className="page_nav_footer_gp">
+				<div>
+					<br/>
+					<h1>Welcome to the About page</h1>
+					<Link to="/home"> Return to the Home page </Link>
+				</div>
+			</div>
 		</div>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,22 @@
 import { Link } from "react-router-dom";
+import appLogo from '../assets/AppLogo.png'
 import './Common.css';
 //
 const HomePage = () => {
   return (
-		<div>
-			<h1>Welcome to the Home page</h1>
-			<Link to="/about"> Click here to go to the About page </Link>
-			<br/><br/>
-			<Link to="/wallet"> Click here to go to the Wallet page </Link>
+		<div className="page_wrap_gp">
+			<div className="page_torso_gp">
+				<img src={appLogo} alt="app logo" />
+			</div>
+			<div className="page_nav_footer_gp">
+				<div>
+					<br/>
+					<h1>Welcome to the Home page</h1>
+					<Link to="/about"> Click here to go to the About page </Link>
+					<br/><br/>
+					<Link to="/wallet"> Click here to go to the Wallet page </Link>
+				</div>
+			</div>
 		</div>
   );
 }
